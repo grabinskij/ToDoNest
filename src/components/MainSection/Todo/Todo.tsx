f364f2366122d688e4808f998d7b82cbe6d49ba0
Bogdan Grabinskij <bogdan.grabinskij@gmail.com>
2024-09-06 21:38:24 +0200

    Add functionality to archive a single todo before clearing the list

--- src/components/MainSection/Todo/Todo.tsx
+++ src/components/MainSection/Todo/Todo.tsx
@@ -4,10 +4,20 @@ import { TodoProps } from '../../../types/types';
 import { RiDeleteBin5Line, RiCheckFill, RiTodoFill, RiEdit2Fill } from 'react-icons/ri'
 import { useDispatch } from 'react-redux';
 import { deleteSingleTodo, setTodoCompletionStatus, editTodo } from '../../../redux/slices/todosSlice';
+import { addToArchive } from '../../../redux/slices/archiveSlice';
 import styles from './Todo.module.css'
 
 const Todo:FC<TodoProps> = ({ todo }) => {
   const dispatch = useDispatch<AppDispatch>();
+
+
+  const deleteSingleTodoAndArchive = (id: string) => {
+    if(confirm('Are your sure you whant to delete this todo?')){
+      const timestamp = new Date()
+      dispatch(deleteSingleTodo(id))
+      dispatch(addToArchive([{todo: todo, archiveTimestamp: timestamp.toISOString(), key: id}]))
+    }
+  }
 
   return (
     <div className={`${styles.todoContainer} ${todo.isCompleted ? styles.completedTodo : ''}`}>
@@ -24,7 +34,7 @@ const Todo:FC<TodoProps> = ({ todo }) => {
     />
     <RiDeleteBin5Line
         className={styles.todoDelete}
-        onClick={() => dispatch(deleteSingleTodo(todo.id))}
+        onClick={() => dispatch(deleteSingleTodoAndArchive(todo.id))}
     />
 </div>
   )
